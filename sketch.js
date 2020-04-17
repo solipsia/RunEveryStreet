aa8ac1ae95eebb822493051fd53143ea40eb18ca
solipsia <63716242+solipsia@users.noreply.github.com>
2020-04-17 17:23:09 +0100

    fixed map scaling

--- sketch.js
+++ sketch.js
@@ -1,6 +1,19 @@
+var map2 = new ol.Map({
+	target: 'map',
+	layers: [
+	  new ol.layer.Tile({
+		source: new ol.source.OSM()
+	  })
+	],
+	view: new ol.View({
+	  center: ol.proj.fromLonLat([0.3192, 51.6297]),
+	  zoom: 17.8
+	})
+  });
+
 function setup() {
 	//createCanvas(windowWidth, windowHeight);
-	let canvas = createCanvas(600, 400);
+	let canvas = createCanvas(1024, 768);
 	canvas.position(0, 0);
 	colorMode(HSB);
 }
@@ -8,5 +21,8 @@ function setup() {
 function draw() {
 	//background(0);
 	rect(10, 10, 10, 10);
-	//console.log(map);
+
+	var extent=[0.3209201,51.628906,0.3352192,51.637436];
+	map2.getView().fit(ol.proj.transformExtent(extent, 'EPSG:4326','EPSG:3857'),map2.getSize());
+	
 }
