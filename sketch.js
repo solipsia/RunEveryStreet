ff4f1618f11e2a7b9ec6ff6a1900e0e7a04f0e6e
solipsia <63716242+solipsia@users.noreply.github.com>
2020-04-17 19:24:17 +0100

    added overpass query

--- sketch.js
+++ sketch.js
@@ -21,8 +21,12 @@ function setup() {
 function draw() {
 	//background(0);
 	rect(10, 10, 10, 10);
-
-	var extent=[0.3209201,51.628906,0.3352192,51.637436];
+	//(minlat,minlon,maxlat,maxlon);51.628906 0.3209201 51.637436 0.3352192
+	var minlat=51.628906;
+	var minlon=0.3209201;
+	var maxlat=51.637436;
+	var maxlon=0.3352192;
+	var extent=[minlon,minlat,maxlon,maxlat];
 	map2.getView().fit(ol.proj.transformExtent(extent, 'EPSG:4326','EPSG:3857'),map2.getSize());
 	
 }
