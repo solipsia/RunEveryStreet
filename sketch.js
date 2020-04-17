c9e8826870d5ddae075fe378411029a42e24065f
solipsia <63716242+solipsia@users.noreply.github.com>
2020-04-17 20:25:27 +0100

    1

--- sketch.js
+++ sketch.js
@@ -1,32 +1,50 @@
 var map2 = new ol.Map({
 	target: 'map',
 	layers: [
-	  new ol.layer.Tile({
-		source: new ol.source.OSM()
-	  })
+		new ol.layer.Tile({
+			source: new ol.source.OSM()
+		})
 	],
 	view: new ol.View({
-	  center: ol.proj.fromLonLat([0.3192, 51.6297]),
-	  zoom: 17.8
+		center: ol.proj.fromLonLat([0.3192, 51.6297]),
+		zoom: 17.8
 	})
-  });
+});
+
+
+var mapWidth = 1024;
+var mapHeight = 768;
+var windowX = mapWidth;
+var windowY = mapHeight + 50;
+let txtoverpassQuery;
 
 function setup() {
 	//createCanvas(windowWidth, windowHeight);
-	let canvas = createCanvas(1024, 768);
+	let canvas = createCanvas(windowX, windowY);
 	canvas.position(0, 0);
 	colorMode(HSB);
+	button = createButton('submit');
+	button.position(10, mapHeight + 5);
+	button.mousePressed(parseOverpassXML);
+	txtoverpassData = createInput();
+	txtoverpassData.position(10, mapHeight + 30);
 }
 
 function draw() {
-	//background(0);
 	rect(10, 10, 10, 10);
-	//(minlat,minlon,maxlat,maxlon);51.628906 0.3209201 51.637436 0.3352192
-	var minlat=51.628906;
-	var minlon=0.3209201;
-	var maxlat=51.637436;
-	var maxlon=0.3352192;
-	var extent=[minlon,minlat,maxlon,maxlat];
-	map2.getView().fit(ol.proj.transformExtent(extent, 'EPSG:4326','EPSG:3857'),map2.getSize());
-	
+	var minlat = 51.628906;
+	var minlon = 0.3209201;
+	var maxlat = 51.637436;
+	var maxlon = 0.3352192;
+	var extent = [minlon, minlat, maxlon, maxlat];
+	map2.getView().fit(ol.proj.transformExtent(extent, 'EPSG:4326', 'EPSG:3857'), map2.getSize());
+}
+
+function parseOverpassXML() {
+	var rawXML = "<mammals><animal>Goat</animal></mammals>";
+
+	//console.log(xml.listChildren());
+	parser = new DOMParser();
+	xmlDoc = parser.parseFromString(rawXML, "text/xml");
+	console.log(xmlDoc.getElementsByTagName("mammals")[0]);
 }
