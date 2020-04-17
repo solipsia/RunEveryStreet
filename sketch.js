82ae74aa702a9e7a4b0a4fe996aef7c15574d529
solipsia <63716242+solipsia@users.noreply.github.com>
2020-04-17 16:10:12 +0100

    added map

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	//createCanvas(windowWidth, windowHeight);
+	let canvas = createCanvas(600, 400);
+	canvas.position(0, 0);
 	colorMode(HSB);
 }
 
 function draw() {
-	background(0);
+	//background(0);
 	rect(10, 10, 10, 10);
 }
