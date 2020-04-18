54d2ed30e6dda332f01fc849959f6dc39366a3e2
solipsia <63716242+solipsia@users.noreply.github.com>
2020-04-18 11:25:47 +0100

    added XML support

--- sketch.js
+++ sketch.js
@@ -17,12 +17,25 @@ var mapHeight = 768;
 var windowX = mapWidth;
 var windowY = mapHeight + 50;
 let txtoverpassQuery;
+let OSMxml;
+var numnodes;
+var numways;
+var nodes ;
+var minlat = 9999;
+var maxlat = -9999;
+var minlon = 9999;
+var maxlon = -9999;
+
+function preload() {
+	OSMxml = loadXML("UpperShenfield.xml");
+}
 
 function setup() {
 	//createCanvas(windowWidth, windowHeight);
 	let canvas = createCanvas(windowX, windowY);
 	canvas.position(0, 0);
 	colorMode(HSB);
+	parseOverpassXML();
 	button = createButton('submit');
 	button.position(10, mapHeight + 5);
 	button.mousePressed(parseOverpassXML);
@@ -32,19 +45,27 @@ function setup() {
 
 function draw() {
 	rect(10, 10, 10, 10);
-	var minlat = 51.628906;
-	var minlon = 0.3209201;
-	var maxlat = 51.637436;
-	var maxlon = 0.3352192;
-	var extent = [minlon, minlat, maxlon, maxlat];
+	var mapminlat = minlat;
+	var mapminlon = minlon;
+	var mapmaxlat = maxlat;
+	var mapmaxlon = maxlon;
+	var extent = [mapminlon, mapminlat, mapmaxlon, mapmaxlat];
 	map2.getView().fit(ol.proj.transformExtent(extent, 'EPSG:4326', 'EPSG:3857'), map2.getSize());
 }
 
 function parseOverpassXML() {
-	var rawXML = "<mammals><animal>Goat</animal></mammals>";
+	var XMLnodes = OSMxml.getChildren('node');
+	var XMLways = OSMxml.getChildren('way');
+	numnodes = XMLnodes.length;
+	numways = XMLways.length;
 
-	//console.log(xml.listChildren());
-	parser = new DOMParser();
-	xmlDoc = parser.parseFromString(rawXML, "text/xml");
-	console.log(xmlDoc.getElementsByTagName("mammals")[0]);
+	for (i = 0; i < numnodes; i++) {
+		lat = XMLnodes[i].getNum("lat");
+		lon = XMLnodes[i].getNum("lon");
+		minlat = min(minlat, lat);
+		maxlat = max(maxlat, lat);
+		minlon = min(minlon, lon);
+		maxlon = max(maxlon, lon);
+	}
+	//console.log(minlat,maxlat,minlon,maxlon)
 }
